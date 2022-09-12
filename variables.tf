602f02304152eaff8e9420d1a831d679d268239c
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 17:25:17 +0530

    updating

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-#variable "aws_access_key" {
-#  type = string
-#}
-#
-#variable "aws_secret_key" {
-#  type = string
-#}
+variable "aws_access_key" {
+ type = string
+}
+
+variable "aws_secret_key" {
+ type = string
+}
 
 variable "access_ip" {
   type = string
