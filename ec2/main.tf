d923a9c0de220dabb245537a465e0ffb2b3207b1
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 16:39:29 +0530

    updating

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,16 +1,16 @@
 # Get Availability Zones
-resource "tls_private_key" "key" {
+resource "tls_private_key" "sree" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 # Generate a Private Key and encode it as PEM.
 resource "aws_key_pair" "key_pair" {
-  key_name   = "key"
-  public_key = tls_private_key.key.public_key_openssh
+  key_name   = "kumar"
+  public_key = tls_private_key.sree.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.key.private_key_pem}' > ./sree.pem"
+    command = "echo '${tls_private_key.sree.private_key_pem}' > ./kumar.pem"
   }
 }
 
