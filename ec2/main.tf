320ecebb5ccc495d80db90074471eec47d0719c8
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 15:11:11 +0530

    updating

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,8 +17,8 @@ resource "aws_key_pair" "key_pair" {
 # Create a EC2 Instance (Ubuntu 20)
 resource "aws_instance" "node" {
   instance_type          = "t2.micro" # free instance
-  ami                    = "ami-0d527b8c289b4af7f"
-  key_name               = aws_key_pair.key_pair.id
+  ami                    = "ami-0ee02425a4c7e78bb"
+key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
 
