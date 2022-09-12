e3ee261b0b611f3972625c8e3f964f12367ab648
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 17:19:32 +0530

    updating

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,33 +1,33 @@
 # Get Availability Zones
-resource "tls_private_key" "qrst" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Generate a Private Key and encode it as PEM.
-#resource "aws_key_pair" "key_pair" {
-#  key_name   = "kumar"
-#  public_key = tls_private_key.sree.public_key_openssh
-#
-#  provisioner "local-exec" {
-#    command = "echo '${tls_private_key.sree.private_key_pem}' > ./kumar.pem"
-#  }
-#}
-
-#resource "tls_private_key" "example" {
+#resource "tls_private_key" "qrst" {
 #  algorithm = "RSA"
 #  rsa_bits  = 4096
 #}
-#variable "key_name" {}
-resource "local_file" "adftstad" {
-  content  = tls_private_key.qrst.private_key_pem
-  filename = "adftstad.pem"
-}
-resource "aws_key_pair" "generated_key" {
-#  key_name   = "var"
-  key_name   = "adftstad"
-  public_key = "${tls_private_key.qrst.public_key_openssh}"
-}
+#
+## Generate a Private Key and encode it as PEM.
+##resource "aws_key_pair" "key_pair" {
+##  key_name   = "kumar"
+##  public_key = tls_private_key.sree.public_key_openssh
+##
+##  provisioner "local-exec" {
+##    command = "echo '${tls_private_key.sree.private_key_pem}' > ./kumar.pem"
+##  }
+##}
+#
+##resource "tls_private_key" "example" {
+##  algorithm = "RSA"
+##  rsa_bits  = 4096
+##}
+##variable "key_name" {}
+#resource "local_file" "adftstad" {
+#  content  = tls_private_key.qrst.private_key_pem
+#  filename = "adftstad.pem"
+#}
+#resource "aws_key_pair" "generated_key" {
+##  key_name   = "var"
+#  key_name   = "adftstad"
+#  public_key = "${tls_private_key.qrst.public_key_openssh}"
+#}
 
 
 #resource "aws_instance" "web" {
@@ -45,7 +45,7 @@ resource "aws_instance" "node" {
   ami                    = "ami-0c2ab3b8efb09f272"
   instance_type          = "t2.micro"
 #  key_name               = aws_key_pair.key_pair.id
-  key_name      = "${aws_key_pair.generated_key.key_name}"
+ # key_name      = "${aws_key_pair.generated_key.key_name}"
 
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
@@ -59,12 +59,12 @@ resource "aws_instance" "node" {
   root_block_device {
     volume_size = 10
   }
-  connection {
-    type        = "ssh"
-    user        = ubuntu
-    private_key = tls_private_key.qrst.private_key_pem
-    host        = aws_instance.node.public_ip
-  }
+#  connection {
+#    type        = "ssh"
+#    user        = ubuntu
+#    private_key = tls_private_key.qrst.private_key_pem
+#    host        = aws_instance.node.public_ip
+#  }
 }
 
 # Create and assosiate an Elastic IP
