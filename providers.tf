bba656a3445449bae9836c99c41e63eda1d9e419
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 17:32:05 +0530

    updating

--- providers.tf
+++ providers.tf
@@ -16,6 +16,6 @@ terraform {
 
 provider "aws" {
   region     = "us-west-2"
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
