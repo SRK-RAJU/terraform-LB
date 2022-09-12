a638d05bdc5cbc3c50b3f8c6c255d56357db3526
91994 <suddala.rajukumar123@gmail.com>
2022-09-12 16:53:19 +0530

    updating

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,20 +5,43 @@ resource "tls_private_key" "sree" {
 }
 
 # Generate a Private Key and encode it as PEM.
-resource "aws_key_pair" "key_pair" {
-  key_name   = "kumar"
-  public_key = tls_private_key.sree.public_key_openssh
+#resource "aws_key_pair" "key_pair" {
+#  key_name   = "kumar"
+#  public_key = tls_private_key.sree.public_key_openssh
+#
+#  provisioner "local-exec" {
+#    command = "echo '${tls_private_key.sree.private_key_pem}' > ./kumar.pem"
+#  }
+#}
 
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.sree.private_key_pem}' > ./kumar.pem"
-  }
+#resource "tls_private_key" "example" {
+#  algorithm = "RSA"
+#  rsa_bits  = 4096
+#}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "kumar"
+  public_key = "${tls_private_key.sree.public_key_openssh}"
 }
+
+
+#resource "aws_instance" "web" {
+#  ami           = "ami-5b673c34"
+#  instance_type = "t2.micro"
+#  key_name      = "${aws_key_pair.generated_key.key_name}"
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
 
 # Create a EC2 Instance (amazon 20)
 resource "aws_instance" "node" {
   ami                    = "ami-0c2ab3b8efb09f272"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.key_pair.id
+#  key_name               = aws_key_pair.key_pair.id
+  key_name      = "${aws_key_pair.generated_key.key_name}"
+
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
 
