8715cb73c91c707536f04d42b55c46a6573a8e24
91994 <suddala.rajukumar123@gmail.com>
2022-09-13 20:17:58 +0530

    updating

--- ec2/main.tf
+++ ec2/main.tf
@@ -71,3 +71,24 @@ resource "aws_instance" "node" {
 resource "aws_eip" "eip" {
   instance = aws_instance.node.id
 }
+
+#resource "aws_lb" "alb-lb" {
+#  name               = "alb-lb-tf"
+#  internal           = false
+#  load_balancer_type = "application"
+#  security_groups    = [aws_security_group.lb_sg.id]
+#  subnets            = [for subnet in aws_subnet.public : subnet.id]
+#
+#  enable_deletion_protection = true
+#
+#  access_logs {
+#    bucket  = aws_s3_bucket.lb_logs.bucket
+#    prefix  = "test-lb"
+#    enabled = true
+#  }
+#
+#  tags = {
+#    Environment = "production"
+#  }
+#}
+
